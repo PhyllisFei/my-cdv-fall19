d060177bc90eb2983396f89d7543ae66c13a9f62
Phyllis <zf9702@gmail.com>
2019-10-08 22:52:06 +0800

    visual draft

--- my-work/dataZineProject/middlescript.js
+++ my-work/dataZineProject/middlescript.js
@@ -1,41 +1,50 @@
 let newdata = [];
+let areadata = [];
 
 function gotData(olddata){
   for (let i=0; i<olddata.length; i++){
-    // console.log(olddata[i]);
     let d = olddata[i];
     let date = d.date;
     let time = d.time;
 
+    let timedate = new Date(date);
+    timedate.setHours(parseInt(String(time).substring(0,2)));
+
+    areadata.push({date: timedate, time: time, name: "family", value: d.family})
     for (let j=0; j < d.family; j++){
       let newd = {date: date, time: time, cause: "family"}
       newdata.push(newd);
     }
+    areadata.push({date: timedate, time: time, name: "friends", value: d.friends})
     for (let j=0; j < d.friends; j++){
       let newd = {date: date, time: time, cause: "friends"}
       newdata.push(newd);
     }
+    areadata.push({date: timedate, time: time, name: "professors", value: d.professors})
     for (let j=0; j < d.professors; j++){
-      let newd = {date: date, time: time, cause: "professors"}
+      let newd = {date: timedate, time: time, cause: "professors"}
       newdata.push(newd);
     }
+    areadata.push({date: timedate, time: time, name: "strangers", value: d.strangers})
     for (let j=0; j < d.strangers; j++){
       let newd = {date: date, time: time, cause: "strangers"}
       newdata.push(newd);
     }
+    areadata.push({date: timedate, time: time, name: "alone", value: d.alone})
     for (let j=0; j < d.alone; j++){
       let newd = {date: date, time: time, cause: "alone"}
       newdata.push(newd);
     }
   }
-  // console.log(newdata);
-  console.log(olddata);
 
 // create svg
+  let w = 2400;
+  let h = 800;
+
   let viz = d3.select("#container")
                   .append("svg")
-                      .attr("width", 1500)
-                      .attr("height", 800)
+                      .attr("width", w)
+                      .attr("height", h)
                       .style("margin-top", 60)
   ;
 
@@ -50,157 +59,240 @@ function gotData(olddata){
       .attr("transform", getPos)
   ;
 
-  // let labels = datagroups.append("text")
-  //   .text(getDate)
-  //   .attr("fill", "black")
-  //   .attr("transform", getPos)
+  // let title = viz.append("text")
+  //                   .text("These are people who make me laugh every day ;)")
+  //                   .attr("fill", "black")
+  //                   .attr("x", 350)
+  //                   .attr("y", 30)
+  //                   .attr("font-size", 25)
   // ;
 
-  let title = viz.append("text")
-                    .text("These are people who make me laugh every day ;)")
+  let text1 = viz.append("text")
+                    .text("Sep 10th")
                     .attr("fill", "black")
-                    .attr("x", 350)
-                    .attr("y", 30)
-                    .attr("font-size", 25)
+                    .attr("x", 65)
+                    .attr("y", 305)
   ;
 
-  let text1 = viz.append("text")
-                    .text("Family")
-                    .attr("fill", "lightblue")
-                    .attr("x", 120)
-                    .attr("y", 500)
-  ;
+  //CANNOT SHOW???
   let text2 = viz.append("test")
-                    .text("Friends")
-                    .attr("fill", "pink")
-                    .attr("x", 120)
-                    .attr("y", 520)
+                    .text("Sep 11th")
+                    .attr("fill", "black")
+                    .attr("x", 240)
+                    .attr("y", 305)
   ;
   let text3 = viz.append("text")
-                    .text("professors")
-                    .attr("fill", "lightgreen")
-                    .attr("x", 120)
-                    .attr("y", 540)
+                    .text("Sep 12th")
+                    .attr("fill", "black")
+                    .attr("x", 410)
+                    .attr("y", 305)
   ;
   let text4 = viz.append("text")
-                    .text("strangers")
-                    .attr("fill", "gray")
-                    .attr("x", 120)
-                    .attr("y", 560)
+                    .text("Sep 13th")
+                    .attr("fill", "black")
+                    .attr("x", 570)
+                    .attr("y", 305)
   ;
   let text5 = viz.append("text")
-                    .text("alone")
-                    .attr("fill", "orange")
-                    .attr("x", 120)
-                    .attr("y", 580)
+                    .text("Sep 14th")
+                    .attr("fill", "black")
+                    .attr("x", 730)
+                    .attr("y", 305)
+  ;
+  let text6 = viz.append("text")
+                    .text("Sep 15th")
+                    .attr("fill", "black")
+                    .attr("x", 900)
+                    .attr("y", 305)
+  ;
+  let text7 = viz.append("text")
+                    .text("Sep 16th")
+                    .attr("fill", "black")
+                    .attr("x", 1070)
+                    .attr("y", 305)
+  ;
+  let text8 = viz.append("text")
+                    .text("Sep 17th")
+                    .attr("fill", "black")
+                    .attr("x", 65)
+                    .attr("y", 550)
+  ;
+  let text9 = viz.append("text")
+                    .text("Sep 18th")
+                    .attr("fill", "black")
+                    .attr("x", 240)
+                    .attr("y", 550)
+  ;
+  let text10 = viz.append("text")
+                    .text("Sep 19th")
+                    .attr("fill", "black")
+                    .attr("x", 410)
+                    .attr("y", 550)
+  ;
+  let text11 = viz.append("text")
+                    .text("Sep 20th")
+                    .attr("fill", "black")
+                    .attr("x", 570)
+                    .attr("y", 550)
+  ;
+  let text12 = viz.append("text")
+                    .text("Sep 21st")
+                    .attr("fill", "black")
+                    .attr("x", 730)
+                    .attr("y", 550)
+  ;
+  let text13 = viz.append("text")
+                    .text("Sep 22nd")
+                    .attr("fill", "black")
+                    .attr("x", 900)
+                    .attr("y", 550)
+  ;
+  let text14 = viz.append("text")
+                    .text("Sep 23th")
+                    .attr("fill", "black")
+                    .attr("x", 1070)
+                    .attr("y", 550)
   ;
 
-  let circle1 = viz.append("svg")
-                      .append("circle")
-                        .attr("r", 4)
-                        .attr("fill", "lightblue")
-                        .attr("x", 100)
-                        .attr("y", 500)
+// Line Area Chart
+  let dateObjectConverter = d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ");
+
+  let alternativeDomainArray = d3.extent(newdata, function(d){
+    return dateObjectConverter(d.date);
+  });
+
+  console.log(alternativeDomainArray);
+
+  /***** X-axis *****/
+  let xPadding = 70;
+  let xScale = d3.scaleTime().domain(alternativeDomainArray).range([xPadding+w/2, w-xPadding]);
+  let xAxis = d3.axisBottom(xScale);
+  let xAxisGroup = viz.append("g").attr("class", "xaxis");
+  xAxisGroup.call(xAxis);
+
+  let xAxisYPos = h-130;
+  xAxisGroup.attr("transform", "translate(0, "+ xAxisYPos +")");
+
+  /***** Y-axis *****/
+  let yScale = d3.scaleLinear()
+                    .domain([0, 18])
+                    .range( [xAxisYPos, 80] );
+
+  let yAxis = d3.axisLeft(yScale);
+  let yAxisGroup = viz.append("g").attr("class", "yaxis");
+  yAxisGroup.call(yAxis);
+  yAxisGroup.attr("transform", "translate( "+ (xPadding + w/2) +", 0)");
+
+  /***** family line area *****/
+  var nestedData = d3.nest()
+      .key(function(d, i) { return d.date;})
+      .entries(areadata)
   ;
-  let circle2 = viz.append("svg")
-                      .append("circle")
-                        .attr("r", 4)
-                        .attr("fill", "pink")
-                        .attr("x", 100)
-                        .attr("y", 520)
+  // console.log(nestedData);
+
+  var mygroups = ["family", "friends", "professors", "strangers", "alone"];
+  var mygroup = [0,1,2,3,4];
+  var stackedData = d3.stack()
+    .keys(mygroup)
+    .value(function(d, key){
+      // console.log(d);
+      // console.log(d.values[key].value);
+      return d.values[key].value
+      }
+    )(nestedData);
+
+  console.log(areadata[0]);
+
+  var color = d3.scaleOrdinal()
+    .domain(mygroups)
+    .range(['lightblue','pink','lightgreen','gray','orange'])
   ;
-  let circle3 = viz.append("svg")
-                      .append("circle")
-                        .attr("r", 4)
-                        .attr("fill", "lightgreen")
-                        .attr("x", 100)
-                        .attr("y", 540)
+
+  let area = d3.area()
+              .x(function(d, i) { return xScale(new Date(d.data.key)); })
+              .y0(function(d) { return yScale(d[0]); })
+              .y1(function(d) { return yScale(d[1]); })
   ;
-  let circle4 = viz.append("svg")
-                      .append("circle")
-                        .attr("r", 4)
-                        .attr("fill", "gray")
-                        .attr("x", 100)
-                        .attr("y", 560)
-  ;
-  let circle5 = viz.append("svg")
-                      .append("circle")
-                        .attr("r", 4)
-                        .attr("fill", "orange")
-                        .attr("x", 100)
-                        .attr("y", 580)
+
+  viz.selectAll("mylayers")
+      .data(stackedData)
+      .enter()
+      .append("path")
+        .style("fill", function(d) { name = mygroups[d.key] ;  return color(name); })
+        .attr("stroke-width", 1.5)
+        .attr("d", area)
   ;
 }
 
 // 7 columns * 2 rows:
 function translate1(){
-  let x = 100 + Math.random()*100;
-  let y = 100 + Math.random()*100;
+  let x = 50 + Math.random()*100;
+  let y = 150 + Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate2(){
-  let x = 250+Math.random()*100;
-  let y = 100+Math.random()*100;
+  let x = 220+Math.random()*100;
+  let y = 150+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate3(){
-  let x = 400+Math.random()*100;
-  let y = 100+Math.random()*100;
+  let x = 390+Math.random()*100;
+  let y = 150+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate4(){
   let x = 550+Math.random()*100;
-  let y = 100+Math.random()*100;
+  let y = 150+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate5(){
-  let x = 700+Math.random()*100;
-  let y = 100+Math.random()*100;
+  let x = 710+Math.random()*100;
+  let y = 150+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate6(){
-  let x = 850+Math.random()*100;
-  let y = 100+Math.random()*100;
+  let x = 880+Math.random()*100;
+  let y = 150+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate7(){
-  let x = 1000+Math.random()*100;
-  let y = 100+Math.random()*100;
+  let x = 1050+Math.random()*100;
+  let y = 150+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate8(){
-  let x = 100+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let x = 50+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate9(){
-  let x = 250+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let x = 220+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate10(){
-  let x = 400+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let x = 390+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate11(){
   let x = 550+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate12(){
-  let x = 700+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let x = 710+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate13(){
-  let x = 850+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let x = 880+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 function translate14(){
-  let x = 1000+Math.random()*100;
-  let y = 300+Math.random()*100;
+  let x = 1050+Math.random()*100;
+  let y = 400+Math.random()*100;
   return "translate (" + x + ", " + y + ")";
 }
 
@@ -211,6 +303,7 @@ function getColor(datapoint, i){
   if(datapoint.cause == "friends"){
     return "pink";
   }
+  //CANNOT SHOW???
   if(datapoint.cause == "professors"){
     return "lightgreen";
   }
@@ -266,9 +359,5 @@ function getPos(datapoint, i){
     return translate14();
   }
 }
-
-// function getDate(datapoint, i){
-//   return datapoint.date;
-// }
 
 d3.json("laughter.json").then(gotData);
