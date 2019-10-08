e2d956c370351c55578e15ded4a473d90a83eb1e
Phyllis <zf9702@gmail.com>
2019-10-08 22:51:39 +0800

    cover page update

--- my-work/dataZineProject/coverscript.js
+++ my-work/dataZineProject/coverscript.js
@@ -1,61 +1,18 @@
-/***** filtered data *****/
-let newdata = [];
+let w = 1200;
+let h = 800;
 
-function gotData(olddata){
-  for(let i=0; i<olddata.length; i++){
-    let d = olddata[i];
-    let date = d.date;
-    let time = d.tine;
+let viz = d3.select("#container")
+                .append("svg")
+                    .attr("width", w)
+                    .attr("height", h)
+;
 
-    for (let j=0; j<d.family; j++){
-      let value = d.family;
-      let newd = {date: date, time: time, value: value}
-      newdata.push(newd);
-    }
-  }
-  console.log(newdata);
-
-/***** line chart *****/
-  var margin = {top: 150, right: 100, bottom: 100, left: 100},
-      width = 1000,
-      height = 500;
-
-  let viz = d3.select("#container")
-      .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// Add X axis
-  var x = d3.scaleTime()
-        .domain( [ 0, newdata.date] )
-        .range([ 0, width ]);
-      viz.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-// Add Y axis
-  var y = d3.scaleLinear()
-    .domain( [0, newdata.value] )
-    .range([ height, 0 ]);
-  viz.append("g")
-    .call(d3.axisLeft(y));
-
-// Add the line
-  viz.append("path")
-    .datum(newdata)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", d3.line()
-      .x(function d(d) { return x(newdata.date) })
-      .y(function d(d) { return x(newdata.value) })
-      )
-}
-
-function d(d){
-    return { date : d3.timeParse("%Y-%m-%d")(newdata.date), value : newdata.value };
-  }
-
-d3.json("laughter.json").then(gotData);
+let text1 = viz.append("text")
+                  .text("Feeling Happy Today?")
+                  .attr("font-family", "Mansalva")
+                  .attr("font-size", 80)
+                  .attr("font-weight","bold")
+                  .attr("fill", "black")
+                  .attr("x", w/2-350)
+                  .attr("y", h/2-10)
+;
